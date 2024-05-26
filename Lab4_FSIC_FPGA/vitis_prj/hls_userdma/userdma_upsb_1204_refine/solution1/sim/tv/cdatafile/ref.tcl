f2568cf59093aa88c0da646eadcd39f3209f5cfb
Roy_Tsai <tsungyingnsysu@gmail.com>
2024-05-26 16:23:33 +0800

    v2_validation

---
+++ Lab4_FSIC_FPGA/vitis_prj/hls_userdma/userdma_upsb_1204_refine/solution1/sim/tv/cdatafile/ref.tcl
@@ -0,0 +1,27 @@
+set depth_list {
+{inStreamTop_V_data_V 64}
+{inStreamTop_V_keep_V 64}
+{inStreamTop_V_strb_V 64}
+{inStreamTop_V_user_V 64}
+{inStreamTop_V_last_V 64}
+{s2m_buf_sts 1}
+{s2m_sts_clear 1}
+{s2m_len 1}
+{s2m_enb_clrsts 1}
+{s2mbuf 1}
+{s2m_err 1}
+{Img_width 1}
+{m2sbuf 1}
+{m2s_buf_sts 1}
+{m2s_sts_clear 1}
+{m2s_len 1}
+{m2s_enb_clrsts 1}
+{outStreamTop_V_data_V 64}
+{outStreamTop_V_keep_V 64}
+{outStreamTop_V_strb_V 64}
+{outStreamTop_V_user_V 64}
+{outStreamTop_V_last_V 64}
+{gmem0 64}
+{gmem1 64}
+}
+set trans_num 2
